dd47e5354d917d0cb824f57f3ff32d82d51df2ff
alenamosk <almoskaeva@gmail.com>
2024-01-01 16:55:53 +0100

    created endpoint to get a single product

--- back/server.ts
+++ back/server.ts
@@ -59,30 +59,32 @@ app.get("/products", async (req, res) => {
   res.send(allProducts);
 });
 
-app.get("/categories", async (req, res) => {
-  const allCategories = await prisma.category.findMany({
+app.get("/product/:id", async (req, res) => {
+  const idAsNumber = parseInt(req.params.id);
+  const oneProduct = await prisma.product.findUnique({
+    where: {
+      id: idAsNumber,
+    },
     select: {
       id: true,
-      catName: true,
-      Product: {
-        select: {
-          id: true,
-          prName: true,
-          expires: true,
-          opened: true,
-          expiresInDays: true,
-          imgUrl: true,
-          user: true,
-          userId: true,
-          category: true,
-          categoryId: true,
-          description: true,
-          important: true,
-        },
-      },
+      prName: true,
+      expires: true,
+      opened: true,
+      expiresInDays: true,
+      imgUrl: true,
+      user: true,
+      userId: true,
+      category: true,
+      categoryId: true,
+      description: true,
+      important: true,
     },
   });
-  res.send(allCategories);
+  if (!oneProduct) {
+    res.status(404).send({ message: "Product with that id not found" });
+    return;
+  }
+  res.send(oneProduct);
 });
 
 app.get("/categories", async (req, res) => {
