5396f947e6e11ae1b411c479966f72671120e387
alenamosk <almoskaeva@gmail.com>
2024-01-04 20:43:19 +0100

    added styling to the registration and login pages

--- front/src/pages/login.tsx
+++ front/src/pages/login.tsx
@@ -51,26 +51,79 @@ const Login = () => {
 
   return (
     <>
-      <LogoLink />
-      <header>
-        <h1>Login</h1>
-      </header>
+      <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+          <img className="mx-auto h-10 w-auto" src="Logo(1).png" alt="Logo" />
+          <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+            Log in to your account
+          </h2>
+        </div>
 
-      <form className="form" onSubmit={handleSubmit(handleFormSubmit)}>
-        <label htmlFor="username">Email</label>
-        <input id="email" {...register("email")} type="text" />
-        {errors.email && <p className="error-msg">{errors.email.message}</p>}
+        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+          <form
+            className="space-y-6"
+            action="#"
+            method="POST"
+            onSubmit={handleSubmit(handleFormSubmit)}
+          >
+            <div>
+              <label
+                htmlFor="email"
+                className="block text-sm font-medium leading-6 text-gray-900"
+              >
+                Email address
+              </label>
+              <div className="mt-2">
+                <input
+                  id="email"
+                  {...register("email")}
+                  name="email"
+                  type="email"
+                  autoComplete="email"
+                  required
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+            {errors.email && (
+              <p className="error-msg">{errors.email.message}</p>
+            )}
 
-        <label htmlFor="password">Password</label>
-        <input id="password" {...register("password")} type="password" />
-        {errors.password && (
-          <p className="error-msg">{errors.password.message}</p>
-        )}
-
-        <button type="submit" id="submit">
-          Submit
-        </button>
-      </form>
+            <div>
+              <div className="flex items-center justify-between">
+                <label
+                  htmlFor="password"
+                  className="block text-sm font-medium leading-6 text-gray-900"
+                >
+                  Password
+                </label>
+              </div>
+              <div className="mt-2">
+                <input
+                  id="password"
+                  {...register("password")}
+                  name="password"
+                  type="password"
+                  autoComplete="current-password"
+                  required
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+            {errors.password && (
+              <p className="error-msg">{errors.password.message}</p>
+            )}
+            <div>
+              <button
+                type="submit"
+                className="flex w-full justify-center rounded-md bg-orange-900 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-orange-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-orange-800"
+              >
+                LOG IN
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
     </>
   );
 };
